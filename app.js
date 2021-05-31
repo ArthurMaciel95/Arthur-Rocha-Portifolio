d720b25f556a303fd288419cae7dd8f00310d39a
ArthurMaciel95 <62079236+ArthurMaciel95@users.noreply.github.com>
2021-05-31 03:33:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,69 +3,6 @@ const closeButton = document.querySelector(".close-button");
 const aside = document.querySelector("aside");
 const contentArea = document.querySelectorAll(".content-area");
 
-//ASIDE ANIMATE BAR
-closeButton.addEventListener("click", () => {
-  aside.classList.toggle("active");
-  if (aside.classList.contains("active")) {
-    closeButton.classList.add("close");
-    contentArea.forEach((content) => {
-      content.classList.add("active");
-    });
-  } else {
-    closeButton.classList.remove("close");
-    contentArea.forEach((content) => {
-      content.classList.remove("active");
-    });
-  }
-});
 
-// TRANSITION SCROLL DIV
-function animaScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
 
-    console.log(sectionTop);
-    if (sectionTop <= 200) {
-      section.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", animaScroll);
-
-const closeButton = document.querySelector(".close-button");
-const aside = document.querySelector("aside");
-const contentArea = document.querySelectorAll(".content-area");
-
-//ASIDE ANIMATE BAR
-closeButton.addEventListener("click", () => {
-  aside.classList.toggle("active");
-  if (aside.classList.contains("active")) {
-    closeButton.classList.add("close");
-    contentArea.forEach((content) => {
-      content.classList.add("active");
-    });
-  } else {
-    closeButton.classList.remove("close");
-    contentArea.forEach((content) => {
-      content.classList.remove("active");
-    });
-  }
-});
-
-// TRANSITION SCROLL DIV
-function animaScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-
-    console.log(sectionTop);
-    if (sectionTop <= 200) {
-      section.classList.add("active");
-    }
-  });
-}
-
-window.addEventListener("scroll", animaScroll);
 
