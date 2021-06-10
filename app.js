d1dd1a1b8132bc39d9de40d66d77015728d7c43f
Arthur Rocha <arthurnmrocha@gmail.com>
2021-06-10 14:50:05 -0300

    fixing bugs in aside bar animation and others

--- app.js
+++ app.js
@@ -4,5 +4,19 @@ const aside = document.querySelector("aside");
 const contentArea = document.querySelectorAll(".content-area");
 
 
+closeButton.addEventListener('click',()=>{
+    aside.classList.toggle('active')
 
+
+    if(aside.classList.contains('active')){
+        contentArea.forEach(content =>{
+            content.classList.add('active')
+        })
+    } else{
+        contentArea.forEach(content =>{
+            content.classList.remove('active')
+        })
+    }
+    
+})
 
